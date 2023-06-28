6d782cf8dd9a67998cbd013521739b911d51af1c
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-28 17:18:36 +0100

    update:finished yearly transport plan , added comments

--- ArraysandLists/Program.cs
+++ ArraysandLists/Program.cs
@@ -130,6 +130,64 @@ namespace arrays
             };
             int number = numbersJagged1[1][2];
             numbersJagged1[1][3] = 50;
+
+            Random random = new Random();
+
+            int TransportTypesCount = Enum.GetNames(typeof(TransportEnum)).Length;
+
+            TransportEnum[][] transport = new TransportEnum[12][];
+
+            for (int month = 1; month <= 12; month++)
+            {
+                int DaysCount = DateTime.DaysInMonth(DateTime.Now.Year, month);
+
+               transport[month - 1] = new TransportEnum[DaysCount];
+
+                for (int day = 1;day <= DaysCount; day++)
+                {
+                   int randomType = random.Next(0,TransportTypesCount);
+
+                   transport[month - 1][day - 1] = (TransportEnum)randomType;
+                }
+            }
+
+            string[] monthNames = GetMonthNames();
+
+            int monthNamesPart = monthNames.Max(n => n.Length) + 2;
+
+            for (int month = 1; month <= transport.Length; month++)
+            {
+                Console.Write($"{monthNames[month - 1]}:".PadRight(monthNamesPart));
+                for (int day = 1; day <= transport[month - 1].Length; day++)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor =
+               transport[month - 1][day - 1].GetColor();
+                    Console.Write(transport[month - 1][day - 1].GetChar());
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(" ");
+                }
+                Console.WriteLine(" ");
+            }
+
+
+
+            static string[] GetMonthNames()
+            {
+                string[] names = new string[12];
+
+                for (int month = 1; month <= 12; month++)
+                {
+                    DateTime firstDay = new DateTime(DateTime.Now.Year, month, 1);
+                    string name = firstDay.ToString("MMMM",
+                        CultureInfo.CreateSpecificCulture("en"));
+                    names[month - 1] = name;
+                }
+                return names;
+            }
+
+
         }
     }
 }
