bb9d84237e1ad612723eaa02f827ba48d2622c32
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-06-29 19:07:36 +0100

    feat:added new files

---
+++ ArraysandLists/CountryEnum.cs
@@ -0,0 +1,7 @@
+
+public enum CountryEnum
+{
+    PL,
+    UK,
+    DE
+}
---
+++ ArraysandLists/Person.cs
@@ -0,0 +1,9 @@
+
+
+
+public class Person
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public CountryEnum Country { get; set; }
+}
