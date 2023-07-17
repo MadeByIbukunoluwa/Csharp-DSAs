f9775d05eeef41e1c555e46ded1db4ac6eb82d5f
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-17 14:53:11 +0100

    feat:added BinaryTreeNode Parent property and GetHeight method

--- VariantsofTrees/BinaryTree/BinaryTreeNode.cs
+++ VariantsofTrees/BinaryTree/BinaryTreeNode.cs
@@ -1,7 +1,10 @@
 
 public class BinaryTreeNode<T> : TreeNode<T>
 {
+    public BinaryTreeNode<T> Parent { get; set; }
+
     public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null, null };
+
     public BinaryTreeNode<T> Left
     {
         get { return (BinaryTreeNode<T>)Children[0]; }
@@ -12,4 +15,16 @@ public class BinaryTreeNode<T> : TreeNode<T>
         get { return (BinaryTreeNode<T>)Children[1]; }
         set { Children[1] = value; }
     }
-}
+    public int GetHeight()
+    {
+        BinaryTreeNode<T> current = this;
+        int height = 1;
+        while (current.Parent != null)
+        {
+            height++;
+            current = current.Parent;
+        }
+        return height;
+    }
+ }
+
