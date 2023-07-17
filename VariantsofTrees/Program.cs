9e6dbbc40a76adc7dde43dc46de563831e7a4b13
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-17 14:54:29 +0100

    update:rebuilt project , modified files

--- VariantsofTrees/Program.cs
+++ VariantsofTrees/Program.cs
@@ -1,9 +1,81 @@
 ﻿
-// if the children property of the nodes didn't exist, we would be 
+// if the children property of the nodes didn't exist, we would be
+
 namespace VariantsOfTrees
 {
     class Trees
     {
+        private static char[][] InitializeVisualization(BinarySearchTree<int> tree, out int width)
+        {
+            int height = tree.GetHeight();
+            width = (int)Math.Pow(2, height) - 1;
+            char[][] console = new char[height * 2][];
+            for (int i = 0; i < height * 2; i++)
+            {
+                console[i] = new char[COLUMN_WIDTH * width];
+            }
+            return console;
+        }
+        private static void DrawlineLeft(BinaryTreeNode<int> node, int row, int column, char[][] console, int columnDelta)
+        {
+            if (node.Left != null)
+            {
+                int startColumnIndex = COLUMN_WIDTH * (column - columnDelta) + 2;
+                int endColumnIndex = COLUMN_WIDTH * column + 2;
+                for (int x = startColumnIndex + 1; x < endColumnIndex; x++)
+                {
+                    console[row + 1][x] = '_';
+                }
+                console[row + 1][startColumnIndex] = '\u250c';
+                console[row + 1][endColumnIndex] = '+';
+            }
+        }
+
+        private static void DrawlineRight(BinaryTreeNode<int> node, int row, int column, char[][] console, int columnDelta)
+        {
+            if (node.Right != null)
+            {
+                int startColumnIndex = COLUMN_WIDTH * columnDelta + 2;
+                int endColumnIndex = COLUMN_WIDTH * (column + columnDelta) + 1;
+                for (int x = startColumnIndex + 1; x < endColumnIndex; x++)
+                {
+                    console[row + 1][x] = '_';
+                }
+                console[row + 1][startColumnIndex] = '+';
+                console[row + 1][endColumnIndex] = '\u2510';
+            }
+        }
+
+        private static void VisualizeNode(BinaryTreeNode<int> node, int row, int column, char[][] console, int width)
+        {
+            if (node != null)
+            {
+                char[] chars = node.Data.ToString().ToCharArray();
+                int margin = (COLUMN_WIDTH - chars.Length) / 2;
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    console[row][COLUMN_WIDTH * column + i + margin] = chars[i];
+                }
+                int columnDelta = (width + 1) / (int)Math.Pow(2, node.GetHeight() + 1);
+                VisualizeNode(node.Left, row + 2, column - columnDelta, console, width);
+                VisualizeNode(node.Right, row + 2, column + columnDelta, console, width);
+                DrawlineLeft(node, row, column, console, columnDelta);
+                DrawlineRight(node, row, column, console, columnDelta);
+            }
+        }
+
+        private static void VisualizeTree(BinarySearchTree<int> tree, string caption)
+        {
+            char[][] console = InitializeVisualization(tree, out int width);
+            VisualizeNode(tree.Root, 0, width / 2, console, width);
+            Console.WriteLine(caption);
+            foreach (char[] row in console)
+            {
+                Console.WriteLine(row);
+            }
+        }
+        private const int COLUMN_WIDTH = 5;
+
         static public void Main(string[] args)
         {
             Tree<int> tree = new Tree<int>();
@@ -184,7 +256,50 @@ namespace VariantsOfTrees
                 Console.WriteLine(text);
                 Console.ForegroundColor = ConsoleColor.Gray;
             }
+
             //Binary Search Trees
+            
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            BinarySearchTree<int> binaryTree = new BinarySearchTree<int>();
+            binaryTree.Root = new BinaryTreeNode<int>()
+            {
+                Data = 100,
+                Parent = null
+            };
+            binaryTree.Root.Left = new BinaryTreeNode<int>()
+            {
+                Data = 50,
+                Parent = binaryTree.Root
+            };
+            binaryTree.Root.Right = new BinaryTreeNode<int>()
+            {
+                Data = 150,
+                Parent = binaryTree.Root
+            };
+            binaryTree.Count = 3;
+            VisualizeTree(binaryTree, "The BST with three nodes(50,100,150)");
+
+
+            binaryTree.Add(75);
+            binaryTree.Add(125);
+            VisualizeTree(binaryTree, "The BST after two nodes (75,125):");
+
+
+            //binaryTree.Remove(25);
+            //VisualizeTree(binaryTree, "The BST after removing the node 25");
+
+
+            Console.Write("Pre-order traversal:\t");
+            Console.Write(string.Join(",  ", binaryTree.Traverse(TraversalEnum.PREORDER).Select(n => n.Data)));
+
+            Console.Write("\nIn-order traversal:\t");
+            Console.Write(string.Join(",  ", binaryTree.Traverse(TraversalEnum.INORDER).Select(n => n.Data)));
+
+            Console.Write("\nPost-order traversal:\t");
+            Console.Write(string.Join(",  ", binaryTree.Traverse(TraversalEnum.POSTORDER).Select(n => n.Data)));
+
+            Console.ReadLine();
         }
     }
 }
